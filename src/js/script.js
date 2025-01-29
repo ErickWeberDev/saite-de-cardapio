1170b9348ece96bbee5482f513d9745a21172638
ErickWeberDev <141372331+ErickWeberDev@users.noreply.github.com>
2025-01-28 22:14:30 -0300

    quase completo

---
+++ src/js/script.js
@@ -0,0 +1,30 @@
+document.addEventListener('DOMContentLoaded', function () {
+  document.querySelectorAll('li').forEach(item => {
+    item.addEventListener('click', event => {
+      const checkbox = item.querySelector('input[type="checkbox"]');
+      if (checkbox) {
+        checkbox.checked = !checkbox.checked;
+        checkbox.dispatchEvent(new Event('change')); // Dispara o evento 'change' manualmente
+      }
+    });
+  });
+
+  const checkboxes = document.querySelectorAll('input[type="checkbox"]');
+  const totalElement = document.querySelector('.Valor p');
+  let total = 0;
+
+  checkboxes.forEach(checkbox => {
+    checkbox.addEventListener('change', function () {
+      const priceElement = this.parentElement.querySelector('.preco');
+      const price = parseFloat(priceElement.textContent.replace('R$', '').replace(',', '.'));
+
+      if (this.checked) {
+        total += price;
+      } else {
+        total -= price;
+      }
+
+      totalElement.textContent = `R$${total.toFixed(2).replace('.', ',')}`;
+    });
+  });
+});
